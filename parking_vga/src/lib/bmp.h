5aa921f86796aa2d9e80ab382c5828525a0e5df2
Jon Ayerdi <jon_ayerdi_33@hotmail.es>
2018-01-10 16:14:53 +0100

    lib template comments

--- parking_vga/src/lib/bmp.h
+++ parking_vga/src/lib/bmp.h
@@ -1,23 +1,34 @@
-/*
- * bmp.h
- *
- *  Created on: 15 Nov 2017
- *      Author: Jon Ayerdi
- */
-
+/*****************************************************************************************
+*                                         bmp.h
+******************************************************************************************
+* AUTHOR: Jon Ayerdi
+*
+* DESCRIPTION: Defines the BMP image file format and provides an
+* interface to parse a BMP file from a stream
+*
+*****************************************************************************************/
 #ifndef SRC_LIB_BMP_H_
 #define SRC_LIB_BMP_H_
 
+/*--------------------------------------------------------------------------------------*/
+/*                                       INCLUDES                                       */
+/*--------------------------------------------------------------------------------------*/
 #include <stddef.h> /* size_t */
 #include <stdint.h> /* uint8_t, uint16_t, uint32_t */
 
 #include "lib/image.h"
 #include "io/stream.h"
 
+/*--------------------------------------------------------------------------------------*/
+/*                                        MACROS                                        */
+/*--------------------------------------------------------------------------------------*/
 #define BMP_STATE_OK 0
 #define BMP_STATE_ERROR_HEADER 1
 #define BMP_STATE_ERROR_STREAM 2
 
+/*--------------------------------------------------------------------------------------*/
+/*                                        TYPES                                         */
+/*--------------------------------------------------------------------------------------*/
 typedef struct bmp_file_header
 {
 	uint16_t signature;
@@ -48,6 +59,18 @@ typedef struct bmp_header
 	bmp_info_header_t info_header;
 } __attribute__((packed)) bmp_header_t;
 
+/*--------------------------------------------------------------------------------------*/
+/*                                  PUBLIC FUNCTIONS                                    */
+/*--------------------------------------------------------------------------------------*/
+
+/** @brief parses a BMP image from an input stream.
+ *  @param input source stream to read the BMP file.
+ *  @param output the resulting parsed image.
+ *  @return result status (BMP_STATE_OK|BMP_STATE_ERROR_HEADER|BMP_STATE_ERROR_STREAM).
+ */
 int bmp_load(input_stream_t input, image_t *output);
 
 #endif /* SRC_LIB_BMP_H_ */
+/****************************************************************************************/
+/*                                       bmp.h
+/****************************************************************************************/
--- parking_vga/src/lib/image.h
+++ parking_vga/src/lib/image.h
@@ -1,18 +1,28 @@
-/*
- * image.h
- *
- *  Created on: 17 Nov 2017
- *      Author: Jon Ayerdi
- */
-
+/*****************************************************************************************
+*                                         image.h
+******************************************************************************************
+* AUTHOR: Jon Ayerdi
+*
+* DESCRIPTION: Defines an image format and basic functions to manipulate it
+*
+*****************************************************************************************/
 #ifndef SRC_LIB_IMAGE_H_
 #define SRC_LIB_IMAGE_H_
 
+/*--------------------------------------------------------------------------------------*/
+/*                                       INCLUDES                                       */
+/*--------------------------------------------------------------------------------------*/
 #include <stddef.h> /* size_t, NULL */
 #include <stdint.h> /* uint32_t */
 
+/*--------------------------------------------------------------------------------------*/
+/*                                        TYPES                                         */
+/*--------------------------------------------------------------------------------------*/
 typedef uint32_t pixel_t; /* ARGB format */
 
+/*--------------------------------------------------------------------------------------*/
+/*                                        MACROS                                        */
+/*--------------------------------------------------------------------------------------*/
 #define PIXEL_ALPHA ((pixel_t) 0xFF000000)
 #define PIXEL_INVISIBLE ((pixel_t) 0x00000000)
 #define PIXEL_RED ((pixel_t) 0x00FF0000)
@@ -27,9 +37,28 @@ typedef struct image
 	pixel_t *pixels;
 } image_t;
 
+/*--------------------------------------------------------------------------------------*/
+/*                                  PUBLIC FUNCTIONS                                    */
+/*--------------------------------------------------------------------------------------*/
+
+/** @brief performs the alpha compositing of value into target.
+ *  @param target pixel over which the alpha compositing is performed.
+ *  @param value the pixel that will be composited into the target.
+ */
 void pixel_alpha_compositing(pixel_t *target, pixel_t value);
 
+/** @brief allocates a pixel buffer for the given image.
+ *  @param image the image where the pixel buffer needs to be allocated.
+ *  @param x width of the image.
+ *  @param y height of the image.
+ */
 int image_alloc(image_t *image, size_t x, size_t y);
+/** @brief frees the allocated memory of an image previously initializad with image_alloc.
+ *  @param image an image previously initializad with image_alloc.
+ */
 void image_free(image_t *image);
 
 #endif /* SRC_LIB_IMAGE_H_ */
+/****************************************************************************************/
+/*                                       image.h
+/****************************************************************************************/
