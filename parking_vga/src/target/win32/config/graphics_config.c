363ff09b976f48e175c1d43f37ad5f647fe8a34c
Jon Ayerdi <jon_ayerdi_33@hotmail.es>
2018-03-15 22:10:32 +0100

    implemented graphics_config for win32

--- parking_vga/src/target/win32/config/graphics_config.c
+++ parking_vga/src/target/win32/config/graphics_config.c
@@ -9,6 +9,8 @@
 /*--------------------------------------------------------------------------------------*/
 #include "config/graphics_config.h"
 
+#include "hal/window_graphics.h"
+
 #include <string.h>
 
 /*--------------------------------------------------------------------------------------*/
@@ -16,8 +18,7 @@
 /*--------------------------------------------------------------------------------------*/
 
 /* Utils */
-#define GRAPHICS_CONFIG_MAP_MODE(NAME,VALUE) { if(!strcmp(mode_name, NAME)) { mode = VALUE; } }
-#define GRAPHICS_CONFIG_MAP_DEVICE(NAME,VALUE) { if(!strcmp(device_name, NAME)) { vga_graphics_config_t _config = VALUE; config = _config; } }
+#define GRAPHICS_CONFIG_MAP_MODE(NAME,X,Y) { if(!strcmp(mode_name, NAME)) { config.x = X; config.y = Y; } }
 
 /*--------------------------------------------------------------------------------------*/
 /*                            PUBLIC FUNCTION DECLARATIONS                              */
@@ -25,6 +26,16 @@
 
 int graphics_config_get(graphics_t *output, const char *device_name, const char *mode_name)
 {
+	window_graphics_config_t config = { .title = device_name, x = 0, y = 0 };
+	GRAPHICS_CONFIG_MAP_MODE("640x480", 640, 480);
+	GRAPHICS_CONFIG_MAP_MODE("800x600", 800, 600);
+	GRAPHICS_CONFIG_MAP_MODE("1280x720", 1280, 720);
+	GRAPHICS_CONFIG_MAP_MODE("1280x1024", 1280, 1024);
+	GRAPHICS_CONFIG_MAP_MODE("1920x1080", 1920, 1080);
+	if(config.x != 0)
+	{
+		return window_graphics_init(output, config);
+	}
 	return GRAPHICS_CONFIG_MODE_NOT_FOUND;
 }
 
